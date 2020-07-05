6a4880fb92a4575fe8ded134d2a07e3a6cf6d242
Mark Zuber <mark@zube.com>
2020-07-05 15:02:56 -0700

    Update build and deploy scripts

--- pibuild.sh
+++ pibuild.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cross build --release --target armv7-unknown-linux-gnueabihf && scp ./target/armv7-unknown-linux-gnueabihf/release/rledsvr pi@ledpi:/home/pi/rledsvr
+ARM_TARGET=armv7-unknown-linux-gnueabihf
+
+
+cross build --release --target $ARM_TARGET && \
+    scp ./target/$ARM_TARGET/release/rledsvr pi@ledpi:/home/pi/rledsvr/rledsvr && \
+    scp ./rledsvr/deploy/rledsvr.service pi@ledpi:/home/pi/rledsvr/rledsvr.service && \
+    scp ./rledsvr/deploy/deploy.sh pi@ledpi:/home/pi/rledsvr/deploy.sh && \
+    ssh pi@ledpi.local '/home/pi/rledsvr/deploy.sh'
 
--- rledsvr/deploy/deploy.sh
+++ rledsvr/deploy/deploy.sh
@@ -3,19 +3,20 @@
 sudo systemctl stop rledsvr
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-RELDIR="$SCRIPT_DIR/../../"
-PROJECT_ROOT_DIR=$(readlink -f $RELDIR)
-BIN_DIR="$PROJECT_ROOT_DIR/target/release"
+# RELDIR="$SCRIPT_DIR/../../"
+# PROJECT_ROOT_DIR=$(readlink -f $RELDIR)
+# BIN_DIR="$PROJECT_ROOT_DIR/target/release"
 
-if [ -d /var/rpiled ]; then
-    sudo rm -rf /var/rpiled
-fi
+# if [ -d /var/rpiled ]; then
+#     sudo rm -rf /var/rpiled
+# fi
 
-sudo mkdir -p /var/rpiled
+# sudo mkdir -p /var/rpiled
 
-sudo cp "$BIN_DIR/rledsvr" /var/rpiled/rledsvr
-sudo cp "$SCRIPT_DIR/rledsvr.service" /var/rpiled/rledsvr.service
+# sudo cp "$BIN_DIR/rledsvr" /var/rpiled/rledsvr
+# sudo cp "$SCRIPT_DIR/rledsvr.service" /var/rpiled/rledsvr.service
 
-sudo systemctl enable /var/rpiled/rledsvr.service
+sudo systemctl daemon-reload
+sudo systemctl enable $SCRIPT_DIR/rledsvr.service
 sudo systemctl start rledsvr
 sudo systemctl status rledsvr
